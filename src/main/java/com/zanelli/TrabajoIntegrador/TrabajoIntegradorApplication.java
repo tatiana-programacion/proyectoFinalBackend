ce7fc381578ad21b982cd77dbf64b937a8c6456e
tatiana-programacion <tatiana.programacion@gmail.com>
2022-07-07 18:42:08 -0300

    Creación de clase GlobalExceptionHandler

--- src/main/java/com/zanelli/TrabajoIntegrador/TrabajoIntegradorApplication.java
+++ src/main/java/com/zanelli/TrabajoIntegrador/TrabajoIntegradorApplication.java
@@ -1,13 +1,16 @@
 package com.zanelli.TrabajoIntegrador;
 
+import org.apache.log4j.PropertyConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class TrabajoIntegradorApplication {
 
 	public static void main(String[] args) {
 
+		PropertyConfigurator.configure("log4j.properties");
 		SpringApplication.run(TrabajoIntegradorApplication.class, args);
 	}
 
